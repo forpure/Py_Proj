f101d5132dfdcaacfcfa1a70e4a71c76f12a78d4
sybmoji <442853852@qq.com>
2016-03-17 07:04:24 +0800

    Finish logRegres.py & New svmMLiA.py - 20160317.

--- PyTest/logRegres.py
+++ PyTest/logRegres.py
@@ -58,3 +58,76 @@ def plotBestFit(weights):
     plt.xlabel('X1')
     plt.ylabel('X2')
     plt.show()
+
+
+def stocGradAscent0(dataMatrix, classLabels):
+    dataMatrix = array(dataMatrix)
+    m, n = shape(dataMatrix)
+    alpha= 0.01
+    weights = ones(n)
+    for i in range(m):
+        h = sigmoid(sum(dataMatrix[i] * weights))
+        error = classLabels[i] - h  # 不再是向量,而是常数
+        weights = weights + alpha * error * dataMatrix[i]
+    return weights
+
+
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    dataMatrix = array(dataMatrix)
+    m, n = shape(dataMatrix)
+    weights = ones(n)
+    for j in range(numIter):
+        dataIndex = range(m)
+        for i in range(m):
+            alpha = 4 / (1.0 + j + i) + 0.01
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex] * weights))
+            error = classLabels[randIndex] - h
+            weights = weights + alpha * error * dataMatrix[randIndex]
+            del(dataIndex[randIndex])
+    return weights
+
+
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX * weights))
+    if prob > 0.5:
+        return 1.0
+    else:
+        return 0.0
+
+
+def colicTest():
+    trainingWeights = ones(21)
+    with open(r'F:\machinelearninginaction\Ch05\horseColicTraining.txt') as frTrain:
+        trainingSet = []
+        trainingLabels = []
+        for line in frTrain:
+            currLine = line.strip().split()
+            lineArr = []
+            for i in range(21):
+                lineArr.append(float(currLine[i]))
+            trainingSet.append(lineArr)
+            trainingLabels.append(float(currLine[21]))
+        trainingWeights = stocGradAscent1(trainingSet, trainingLabels, 500)
+    errorCount = 0.0
+    numTestVec = 0.0
+    with open(r'F:\machinelearninginaction\Ch05\horseColicTest.txt') as frTest:
+        for line in frTest:
+            numTestVec += 1.0
+            currLine = line.strip().split()
+            lineArr = []
+            for i in range(21):
+                lineArr.append(float(currLine[i]))
+            if int(classifyVector(array(lineArr), trainingWeights)) != int(currLine[21]):
+                errorCount += 1
+    errorRate = (float(errorCount) / numTestVec)
+    print 'the error rate of this  test is: %f' % errorRate
+    return errorRate
+
+
+def multiTest():
+    numTests = 10
+    errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print 'after %d iterations the average error rate is: %f' % (numTests, errorSum / float(numTests))
